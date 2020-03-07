44c828829096404c4ee84589805b3240819d1c04
Anand Rajaram <anandrajaram21@gmail.com>
2020-03-07 17:08:12 +0530

    Finished My first React and blockccin project

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,68 @@
+import web3 from "./web3";
+
+const address = "0xF4B03c002FA800343c9A23eBf99e70EA3ABf3c83";
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "random",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor"
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
--- lottery/deploy.js
+++ lottery/deploy.js
@@ -10,13 +10,13 @@ const web3 = new Web3(provider)
 
 const deploy = async () => {
 	const accounts = await web3.eth.getAccounts();
-
 	console.log('Attempting to deploy from account', accounts[0])
 
 	const result = await new web3.eth.Contract(JSON.parse(interface))
-	.deploy({ data: bytecode })
+	.deploy({ data: '0x' + bytecode })
 	.send({ gas: '1000000', from: accounts[0] });
 
+	console.log(interface);
 	console.log('Contract deployed to', result.options.address);
 }
 
